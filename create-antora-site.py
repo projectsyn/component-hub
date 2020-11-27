de43f740f31cfa6676444baa142c1f5ad474e2bd
Adrian Kosmaczewski <adrian@vshn.ch>
2020-11-27 14:35:24 +0100

    Displaying the index page in alphabetical order

--- create-antora-site.py
+++ create-antora-site.py
@@ -4,10 +4,12 @@ import shutil
 from cookiecutter.main import cookiecutter
 
 root_path = 'commodore-components-hub'
+deployment_path = '%s/deployment' % root_path
 ebook_path = '%s/docs/%s.adoc' % (root_path, root_path)
 ebook_folder_path = '%s/docs/ebook' % root_path
 images_path = '%s/docs/modules/ROOT/assets/images' % root_path
 pages_path = '%s/docs/modules/ROOT/pages' % root_path
+gitlab_ci_path = '%s/.gitlab-ci.yml' % root_path
 
 # Uses Cookiecutter to generate a new Antora documentation site
 params = {
@@ -24,6 +26,8 @@ cookiecutter('git@git.vshn.net:vshn/antora-bootstrap',
 
 # Remove extra files
 os.remove(ebook_path)
+os.remove(gitlab_ci_path)
+shutil.rmtree(deployment_path)
 shutil.rmtree(ebook_folder_path)
 shutil.rmtree(images_path)
 shutil.rmtree(pages_path)
