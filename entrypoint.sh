286130fff29041b1b0462184923a4ad22603ec65
Adrian Kosmaczewski <adrian@vshn.ch>
2021-01-14 16:27:57 +0100

    Fixed script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 python create-antora-site.py
-python generate-nav.py > commodore-components-hub/docs/modules/ROOT/nav.adoc
-python generate-index.py > commodore-components-hub/docs/modules/ROOT/pages/index.adoc
-python generate-playbook.py > commodore-components-hub/playbook.yml
+GITHUB_TOKEN=$GITHUB_TOKEN python generate-nav.py > commodore-components-hub/docs/modules/ROOT/nav.adoc
+GITHUB_TOKEN=$GITHUB_TOKEN python generate-index.py > commodore-components-hub/docs/modules/ROOT/pages/index.adoc
+GITHUB_TOKEN=$GITHUB_TOKEN python generate-playbook.py > commodore-components-hub/playbook.yml
 cd commodore-components-hub || exit
 git add .
 git commit -m "First commit"
