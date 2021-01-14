49c67c73e23ab75946a4692e55730fd68c5dd1a3
Adrian Kosmaczewski <adrian@vshn.ch>
2021-01-14 16:20:37 +0100

    Fixed shellcheck warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 python create-antora-site.py
-python generate-nav.py > commodore-components-hub/docs/modules/ROOT/nav.adoc && \
-python generate-index.py > commodore-components-hub/docs/modules/ROOT/pages/index.adoc && \
-python generate-playbook.py > commodore-components-hub/playbook.yml && \
-cd commodore-components-hub
+python generate-nav.py > commodore-components-hub/docs/modules/ROOT/nav.adoc
+python generate-index.py > commodore-components-hub/docs/modules/ROOT/pages/index.adoc
+python generate-playbook.py > commodore-components-hub/playbook.yml
+cd commodore-components-hub || exit
 git add .
 git commit -m "First commit"
 make html
